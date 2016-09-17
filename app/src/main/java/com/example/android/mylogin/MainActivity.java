a28475b95114ab61f8ced47354235612c5be3828
djsreeraj <sreeraj011m@gmail.com>
2016-09-18 00:18:30 +0530

    Improved  UI , added login and registration

--- app/src/main/java/com/example/android/mylogin/MainActivity.java
+++ app/src/main/java/com/example/android/mylogin/MainActivity.java
@@ -34,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void btnRegister(View view) {
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -41,18 +46,4 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/com/example/android/mylogin/Register.java
+++ app/src/main/java/com/example/android/mylogin/Register.java
@@ -1,19 +1,15 @@
 package com.example.android.mylogin;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
 /**
  * Created by Sreeraj M on 9/17/2016.
  */
-public class Login extends AppCompatActivity {
-
+public class Register extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
-        Toast.makeText(this, "Login Successfull...!", Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.register);
     }
 }
